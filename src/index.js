8676245e8533cebe8ed9a69d018b447f982eb0f5
Sonu <sonu3660@gmail.com>
2020-08-01 02:43:05 +0530

    Add App component by function component and class component both and also initalized and update state system

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// FUNCTION Based Component ***
+// const App = () => {
+//    window.navigator.geolocation.getCurrentPosition(
+//       (position) => console.log(position),
+//       (err) => console.log(err)
+//    );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//    return (
+//       <div>
+//          <h4>Latitude: </h4>
+//       </div>
+//    );
+// };
+
+// CLASS Based Component ***
+class App extends React.Component {
+   constructor(props) {
+      super(props);
+
+      // This is the only time we do direct assignment
+      this.state = { lat: null };
+
+      window.navigator.geolocation.getCurrentPosition(
+         (position) => {
+            //  we called setState!!!
+            this.setState({ lat: position.coords.latitude });
+
+            // we did not!!!
+            // this.state.lat = position.coords.latitude;
+         },
+         (err) => console.log(err)
+      );
+   }
+
+   // React says we have to define render!!
+   render() {
+      return <div>Latitude: {this.state.lat}</div>;
+   }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
