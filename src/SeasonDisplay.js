3218bdf1bc06138b628bab60ee105399586cbd40
Sonu <sonu3660@gmail.com>
2020-08-02 15:44:46 +0530

    Add seasonConfig objectand remove ternary object

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,8 +1,42 @@
 import React from "react";
 
+const seasonConfig = {
+   summer: {
+      text: "Let's hit the Beach",
+      iconName: "sun",
+   },
+   winter: {
+      text: "Burr, It is Chilly",
+      iconName: "snowflake",
+   },
+};
+
+const getSeason = (lat, months) => {
+   if (months > 2 && months < 9) {
+      // lat>0 then Northern Hemisphere else Southern Hemisphere
+      return lat > 0 ? "summer" : "winter";
+   } else {
+      return lat > 0 ? "winter" : "summer";
+   }
+};
+
 const SeasonDisplay = (props) => {
-   console.log(props.lat);
-   return <div>Display Season</div>;
+   const season = getSeason(props.lat, new Date().getMonth());
+   // console.log(season);
+   // const text =
+   //    season === "winter" ? "Burr, It is Chilly" : "Let's hit the beach";
+   // const icon = season === "winter" ? "snowflake" : "sun";
+   // console.log(icon);
+
+   const { text, iconName } = seasonConfig[season];
+
+   return (
+      <div>
+         <i className={`${iconName} icon`} />
+         <h1>{text}</h1>
+         <i className={`${iconName} icon`} />
+      </div>
+   );
 };
 
 export default SeasonDisplay;
